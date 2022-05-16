da987d8ec21d8d59c57a102704aba1ec0135a2ae
LeoCal4 <linkindayd@gmail.com>
2022-05-16 10:08:36 +0200

    Created custom dataset and model training

---
+++ src/datatuner/lm/custom/datatuner_dataset.py
@@ -0,0 +1,172 @@
+import os
+from typing import Dict, List, Tuple
+import json
+from tqdm import tqdm
+from torch.utils.data import Dataset, DataLoader
+from transformers.tokenization_utils import PreTrainedTokenizer
+from transformers import T5Tokenizer, GPT2Tokenizer
+
+
+def get_raw_dataset(filename: str, task_config: Dict) -> List[Dict]:
+    """Reads the dataset from the file specified at filename, using 
+    the information contained in task_config.
+
+    Args:
+        filename (str)
+        task_config (Dict)
+
+    Returns:
+        List[Dict]: a list of dicts each containing a "data" and a "text" field for 
+            each data point
+    """
+    #* open the dataset file
+    with open(filename, "r") as f:
+        data = json.load(f)
+
+    #* get the name of the text fields 
+    text_fields = [x for x in task_config["data_shape"] if x["type"] == "text"]
+    NEW_TEXT_FIELD_NAMES = ["data", "text"]
+
+    #* iterate over data
+    raw_dataset = []
+    for _, raw_data_point in enumerate(tqdm(data)):
+        item = {}
+        #* iterate on the text field of the current data point
+        #*  the first one is always the data, the second is the sentence 
+        for i, text_field in enumerate(text_fields):
+            item[NEW_TEXT_FIELD_NAMES[i]] = raw_data_point[text_field["id"]]
+
+        #* add any needed extra_field
+        if "extra_fields" in task_config: # found in webnlg
+            for extra_field in task_config["extra_fields"]:
+                item[extra_field] = raw_data_point[extra_field]
+        
+        raw_dataset.append(item)
+    return raw_dataset
+
+
+def read_special_tokens(task_config: Dict, special_tokens_file_path: str) -> List[str]:
+    """Read special tokens from file and from the task configuration"""
+    tokens = []
+    #* Add any special tokens indicated in the file
+    with open(special_tokens_file_path, "r") as f:
+        tokens += [x.strip() for x in f.readlines() if x.strip()]
+    if task_config is not None:
+        # add any special tokens defined in the tokenization config
+        for item in task_config["data_shape"]:
+            if item["type"] == "special":
+                tokens += [item["id"]]
+        if "extra_special_tokens" in task_config:
+            tokens.extend(task_config["extra_special_tokens"])
+    #* add base tokens
+    tokens += ["<data>", "<text>"]
+    print(f"read {len(tokens)} special tokens from {special_tokens_file_path} and base tokens")
+    #? add mask token?
+    return tokens
+
+
+class DatatunerDataset(Dataset):
+    def __init__(self, raw_dataset: List[Dict], tokenizer: PreTrainedTokenizer, is_validation: bool=False,
+            data_special_token: str = "data", text_special_token: str = "text",
+            max_source_len: int = None, max_target_len: int = None) -> None:
+        self.raw_dataset = raw_dataset
+        self.tokenizer = tokenizer
+        self.is_validation = is_validation
+        self.data_special_token = data_special_token
+        self.text_special_token = text_special_token
+        self.max_source_len = max_source_len
+        self.source_padding_strategy = "max_length" if self.max_source_len else "longest"
+        self.max_target_len = max_target_len #! unused for now
+        self.processed_sources = []
+        self.processed_targets = []
+        self.apply_tokenizer_to_raw_dataset()
+
+    def apply_tokenizer_to_raw_dataset(self):
+        """Builds the input for the model, normally processing the source (tokenization + conversion to id + padding)
+            and building the labels as such:
+            - tokenization
+            - pad to the left to match the len of the related source
+            - pad to the right together with the source
+        """
+        total_data = []
+        total_targets = []
+        for entry in self.raw_dataset:
+            if not self.is_validation:
+                #* build and tokenize the source string (data token + data + text token + text)
+                source_string = f"<{self.data_special_token}> {entry['data']} <{self.text_special_token}> {entry['text'][-1]}"
+                source_string = self.tokenizer(source_string)["input_ids"]
+                total_data.append(source_string)
+                #* tokenize the text part for the target
+                only_text_tokens = self.tokenizer(entry["text"][-1])["input_ids"]
+                #* manually pad the target string on the left to make it the same size of the source text
+                target_string = [
+                    self.tokenizer.pad_token_id for _ in range(len(source_string) - len(only_text_tokens))
+                    ] + only_text_tokens
+                total_targets.append(target_string)
+            else:
+                #* build and tokenize the complete source string (data token + data + text token + text)
+                complete_source_string = f"<{self.data_special_token}> {entry['data']} <{self.text_special_token}> {entry['text'][-1]}"
+                complete_source_string = self.tokenizer(complete_source_string)["input_ids"]
+                #* build and tokenize the validation source string (data token + data + text token)
+                val_source_string = f"<{self.data_special_token}> {entry['data']} <{self.text_special_token}>"
+                val_source_string = self.tokenizer(val_source_string)["input_ids"]
+                #* manually pad the source string on the right to make it the same size of the complete source text
+                val_source_string = val_source_string + [
+                    self.tokenizer.pad_token_id for _ in range(len(complete_source_string) - len(val_source_string))
+                    ] # TODO NEED TO UPDATE ATTENTION MASKS TOO
+                total_data.append(val_source_string)
+                #* tokenize the text part for the target
+                only_text_tokens = self.tokenizer(entry["text"][-1])["input_ids"]
+                #* manually pad the target string on the left to make it the same size of the source text
+                target_string = [
+                    self.tokenizer.pad_token_id for _ in range(len(complete_source_string) - len(only_text_tokens))
+                    ] + only_text_tokens
+                total_targets.append(target_string)
+        #* finish using batch_encode_plus, since __call__ does not accept input_ids
+        self.processed_sources = self.tokenizer.batch_encode_plus(
+            total_data, padding=self.source_padding_strategy, return_tensors="pt", is_split_into_words=True, truncation=True, 
+            max_length=self.max_source_len
+        )
+        #* pad labels to the same len of the source 
+        #! change the pad token to the mask one?
+        target_len = len(self.processed_sources["input_ids"][0])
+        self.processed_targets = self.tokenizer.batch_encode_plus(
+            total_targets, padding="max_length", max_length=target_len, return_tensors="pt", is_split_into_words=True
+        )
+
+    def __len__(self) -> int:
+        return len(self.raw_dataset)
+
+    def __getitem__(self, idx) -> Tuple[Dict]:
+        item = {}
+        item["source_input_ids"] = self.processed_sources.data["input_ids"][idx]
+        item["source_attention_mask"] = self.processed_sources.data["attention_mask"][idx]
+        item["target_input_ids"] = self.processed_targets.data["input_ids"][idx]
+        item["target_attention_mask"] = self.processed_targets.data["attention_mask"][idx]
+        return item
+
+
+def get_data_loaders(base_dataset_path: str, task_config: Dict, tokenizer: PreTrainedTokenizer,
+        train_batch_size: int = 8, val_batch_size: int = 1,
+        max_source_len: int = None, max_target_len: int = None) -> Dict[str, DataLoader]:
+    data_loaders = {}
+    for dataset_type in ["train", "validation"]:
+        current_dataset_path = os.path.join(base_dataset_path, f"{dataset_type}.json")
+        raw_dataset = get_raw_dataset(current_dataset_path, task_config)
+        current_dataset = DatatunerDataset(raw_dataset, tokenizer, is_validation=bool(dataset_type=="validation"),
+            max_source_len=max_source_len, max_target_len=max_target_len)
+        batch_size = train_batch_size if dataset_type == "train" else val_batch_size
+        data_loaders[dataset_type] = DataLoader(
+            current_dataset, batch_size=batch_size, shuffle=bool(dataset_type=="train"))
+    return data_loaders["train"], data_loaders["validation"]
+
+
+if __name__ == "__main__":
+    base_dataset_path = r"/content/drive/MyDrive/Tesi/datasets/webnlg/validation.json"
+    task_config_path = r"/content/drive/MyDrive/Tesi/datasets/webnlg/webnlg_task_config.json"
+    task_config = json.load(open(task_config_path, "r"))
+    special_tokens_path = r"/content/drive/MyDrive/Tesi/datasets/webnlg/special_tokens.txt"
+    special_tokens = read_special_tokens(task_config, special_tokens_path)
+    tokenizer = T5Tokenizer.from_pretrained("t5-small")
+    tokenizer.add_tokens(special_tokens)
+    data_loaders = get_data_loaders(base_dataset_path, task_config, tokenizer)
---
+++ src/datatuner/lm/custom/train.py
@@ -0,0 +1,227 @@
+import logging
+import datetime
+import argparse
+import random
+import numpy as np
+import json
+from typing import *
+import os
+
+import torch
+import torch.nn as nn
+from tqdm import tqdm
+from transformers import (
+    AdamW,
+    T5ForConditionalGeneration,
+    T5Tokenizer,
+    get_linear_schedule_with_warmup
+)
+
+from datatuner.lm.custom import datatuner_dataset
+from nltk.translate.bleu_score import sentence_bleu
+
+logging.basicConfig(level=logging.INFO)
+log = logging.getLogger(__file__)
+
+# Configuration details. These could be passed as command line arguments but are done this way
+# for simplicity.
+
+def parse_arguments() -> argparse.Namespace:
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--seed", type=int, default=42, help="Seed")
+    parser.add_argument("--base_dataset_path", type=str, help="Path of the dataset.")
+    parser.add_argument("--task_config_path", type=str, help="Path to the tokenization config file")
+    parser.add_argument("--special_tokens_path", type=str, default=None, help="Path to the special tokens file")
+    parser.add_argument("--train_params_path", type=str, help="JSON file with training parameters.")
+    parser.add_argument("--save_dir_path", type=str, default="./save", help="Path to the save directory.")
+    parser.add_argument("--device", type=str, default="cuda" if torch.cuda.is_available() else "cpu", help="Device (cuda or cpu)")
+    parser.add_argument("--model_name", type=str, default="t5-small", help="Short name of the model")
+    parser.add_argument("--train_batch_size", type=int, default=8, help="Batch size for training")
+    parser.add_argument("--val_batch_size", type=int, default=8, help="Batch size for validation")
+    parser.add_argument("--lr", type=float, default=1e-4, help="Learning rate")
+    parser.add_argument("--adam_epsilon", type=float, default=1e-8, help="Learning rate")
+    parser.add_argument("--max_grad_norm", type=float, default=1.0, help="Clipping gradient norm")
+    parser.add_argument("--epochs", type=int, default=2, help="Number of training epochs")
+    parser.add_argument("--warmup_steps", default=0, type=int, help="Linear warmup over warmup_steps.")
+    # parser.add_argument("--patience", type=int, default=1, help="patience parameter for early stopping")
+    return parser.parse_args()
+
+
+def set_seed(seed):
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    if torch.cuda.is_available():
+        torch.cuda.manual_seed_all(seed)
+
+
+def forward(model, batch, device: str, pad_token_id: int = 0):
+    """When we call model() with labels, they will be:
+    - automatically right shifted by 1 (for teacher forcing)
+    - prepended by BOS=Beginning of sequence which is a PAD token
+    - any token that was -100 will be masked_fill_ to <pad> for teacher forcing
+
+    Args:
+        model (_type_)
+        device (str)
+        batch (_type_)
+        pad_token_id (int): defaults to 0
+
+    Returns:
+        float: loss value
+    """
+    source_ids = batch["source_input_ids"].to(device, dtype=torch.long)
+    source_mask = batch["source_attention_mask"].to(device, dtype=torch.long)
+    target_ids = batch["target_input_ids"].to(device, dtype=torch.long)
+    #* padded ids are set to -100, so that they are ignored during loss calculation
+    target_ids[target_ids[: ,:] == pad_token_id] = -100
+    label_ids = target_ids.to(device)
+    out_dict = model(source_ids, attention_mask=source_mask, labels=label_ids, return_dict=True)
+    loss = out_dict[0]
+    return loss
+
+
+def main():
+    #* set seed and args
+    log.info("Parsing arguments")
+    args = parse_arguments()
+    set_seed(args.seed)
+
+    #* check if there are any additional training args and add them to args
+    log.info(f"Checking additional parameters at {args.train_params_path}")
+    if args.train_params_path:
+        train_params = json.load(open(args.train_params_path, "r"))
+        for param in train_params:
+            if not hasattr(args, param):
+                setattr(args, param, train_params[param])
+
+    #* load model and tokenizer
+    log.info(f"Loading model and tokenizer")
+    model = T5ForConditionalGeneration.from_pretrained(args.model_name)
+    tokenizer = T5Tokenizer.from_pretrained(args.model_name)
+
+    #* check and eventually update special_tokens_path
+    if not args.special_tokens_path:
+        args.special_tokens_path = os.path.join(args.base_dataset_path, "special_tokens.txt")
+
+    #* load task_config and special_tokens
+    log.info(f"Loading task config and special tokens")
+    task_config = json.load(open(args.task_config_path, "r"))
+    special_tokens = datatuner_dataset.read_special_tokens(task_config, args.special_tokens_path)
+    tokenizer.add_tokens(special_tokens)
+    model.resize_token_embeddings(len(tokenizer))
+    model.to(args.device)
+
+    #* load dataset as DataLoaders
+    log.info(f"Loading dataset from {args.base_dataset_path}")
+    train_loader, val_loader = datatuner_dataset.get_data_loaders(
+        args.base_dataset_path, task_config, tokenizer, 
+        train_batch_size=args.train_batch_size, val_batch_size=args.val_batch_size)
+    log.info(f"Train size: {len(train_loader)} - Val size: {len(val_loader)}")
+
+    #* set up optimizer and scheduler
+    num_train = len(train_loader.dataset)
+    total_train = num_train * args.epochs
+    total_steps = ( (num_train // args.train_batch_size) * args.epochs)     # num times that optim.step() will be called
+    optimizer = AdamW(model.parameters(), lr=args.lr, eps=args.adam_epsilon)
+    scheduler = get_linear_schedule_with_warmup(optimizer, num_warmup_steps=args.warmup_steps, num_training_steps=total_steps)
+    log.info(f'Device: {args.device}\n'
+             f'Total steps: {total_steps}\n'
+             f'Total train (# training examples * epochs): {total_train}\n')
+
+    config_str = "\n"
+    for k, v in vars(args).items():
+        config_str += f"{k}: {v}\n"
+    config_str += f"Save directory: {args.save_dir_path}\n"
+    log.info(config_str)
+
+    log.info("Starting training")
+    epoch = 0
+    step = 0 # number of total examples we have done (will be epoch * len(data_set) at end of each epoch)
+    while epoch < args.epochs:
+        epoch += 1
+        #* train
+        model.train()
+        with torch.enable_grad(), tqdm(total=num_train) as progress_bar:
+            for batch_num, batch in enumerate(train_loader):
+                #* forward
+                batch_size = len(batch["source_input_ids"])
+                loss = forward(model, batch, args.device, pad_token_id=tokenizer.pad_token_id)
+                loss_val = loss.item()      # get the item since loss is a tensor
+                #* backward
+                optimizer.zero_grad()
+                loss.backward()
+                nn.utils.clip_grad_norm_(model.parameters(), args.max_grad_norm)
+                optimizer.step()
+                optimizer.zero_grad() #! is this needed/harmful? TODO check
+                scheduler.step()
+                #* log info
+                step += batch_size
+                progress_bar.update(batch_size)
+                progress_bar.set_postfix(epoch=epoch, loss=loss_val)
+                # tbx.add_scalar('train/loss', loss_val, step)
+                # tbx.add_scalar('train/LR', optimizer.param_groups[0]['lr'], step)
+
+        #* evaluate
+        log.info(f'Evaluating at step {step}...')
+        intermediate_predictions = []   
+        bleus = []                  
+        num_val = len(val_loader.dataset)
+        model.eval()
+        with torch.no_grad(), tqdm(total=num_val) as progress_bar:
+            for batch_num, batch in enumerate(val_loader):
+                batch_size = len(batch["source_input_ids"])
+
+                # #* evaluation for loss fcn
+                # loss, _ = forward(model, batch, args.device)     # loss, logits, but don't need logits
+                # loss_meter.update(loss.item(), batch_size)  # loss.item() since it's a tensor
+
+                #* generate for token matches
+                source_ids = batch["source_input_ids"].to(args.device, dtype=torch.long)
+                source_mask = batch["source_attention_mask"].to(args.device, dtype=torch.long)
+                generated_ids = model.generate(source_ids, attention_mask=source_mask, max_length=200) #! hardcoded length TODO
+
+                #* collect some stats
+
+                #* save for qualitative analysis
+                original_data_inputs = tokenizer.batch_decode(batch["source_input_ids"], skip_special_tokens=True)
+                original_text_targets = tokenizer.batch_decode(batch["target_input_ids"], skip_special_tokens=True)
+                outputs_decoded = tokenizer.batch_decode(generated_ids, skip_special_tokens=True)
+                for source, generated in zip(original_text_targets, outputs_decoded):
+                    bleus.append(sentence_bleu([source], generated))
+                current_predictions = list(zip(original_data_inputs, original_text_targets, outputs_decoded))
+                intermediate_predictions.extend(current_predictions)
+
+                #* print one batch of generations for qualitative assessment
+                if batch_num == 0:
+                    for _, orig_input, actual_output in current_predictions[:1]:
+                        # print(f"Source: {orig_input}\n\tActual: {actual_output}")
+                        log.info(f"\nSource: {orig_input}\n"
+                                 f"\t Actual: {actual_output}")
+
+                #* log info
+                progress_bar.update(batch_size)
+                # progress_bar.set_postfix(NLL=loss_meter.avg)
+            log.info(f"\nAverage BLEU at end of epoch {epoch}: {sum(bleus)/float(len(bleus)):.3f}")
+
+        #* save predictions for qualititative analysis
+        to_write = ""
+        for prediction_pair in intermediate_predictions:
+            data, source, generated = prediction_pair
+            to_write += f"DATA: {data}\nOG: {source}\nGEN: {generated}\n\n"
+        dataset_name = args.base_dataset_path.split("/")[-1]
+        with open(f"predictions_{dataset_name}_{datetime.datetime.now().timestamp()}.txt", "w") as f:
+            f.write(to_write)
+
+        # util.save_preds(pred_list_all, record_dir)
+        # util.save_preds(pred_list_correct, record_dir, file_name="preds_correct.csv")
+        # results_list = [('exact_match_with_eos', total_matches_with_eos_ct),
+        #                 ('exact_match_no_eos', total_matches_no_eos_ct)]
+        # results = OrderedDict(results_list)
+        # Log to console
+        # results_str = ', '.join(f'{k}: {v:05.2f}' for k, v in results.items())
+        # log.info(f'Dev {results_str}')
+
+
+if __name__ == '__main__':
+    main()
