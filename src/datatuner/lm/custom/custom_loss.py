85421624f9759442da8950891ac24f4b4d2db552
LeoCal4 <linkindayd@gmail.com>
2022-06-03 17:59:31 +0200

    Made smloss differentiable

--- src/datatuner/lm/custom/custom_loss.py
+++ src/datatuner/lm/custom/custom_loss.py
@@ -1,59 +1,109 @@
 import logging
-import math
-import re
 from typing import List, Union
-import nltk
 import torch
 
 logging.basicConfig(level=logging.INFO)
 log = logging.getLogger(__file__)
 
 
-def tensor_intersection(a: torch.Tensor, b: torch.Tensor) -> torch.Tensor:
-    a_cat_b, counts = torch.cat([a, b]).unique(return_counts=True)
-    return a_cat_b[torch.where(counts.gt(1))]
-
-
-def semantic_fidelity_loss(source_data, target_texts, model_outputs: List[str],
-                            missing_data_token_weight: Union[float, torch.Tensor] = 0.5,
-                            token_difference_weight: Union[float, torch.Tensor] = 0.5) -> torch.Tensor:
-    """_summary_
+def tensor_intersection(data: torch.Tensor, predicted: torch.Tensor) -> torch.Tensor:
+    """Differentiable tensor intersection. Searches the elements of predicted in data,
+    then returns a sliced version of predicted, containing only the elements in common.
 
     Args:
-        source_data ()
-        target_texts ()
-        model_outputs (List[str])
-        missing_data_token_weight (Union[float, torch.Tensor], optional): Either a float or a single-item tensor. Defaults to 0.5.
-        token_difference_weight (Union[float, torch.Tensor], optional): Either a float or a single-item tensor. Defaults to 0.5.
+        data (torch.Tensor)
+        predicted (torch.Tensor)
+
+    Returns:
+        torch.Tensor: tensor containing the intersection of data and predicted
+    """
+    # a_cat_b, counts = torch.cat([a, b]).unique(return_counts=True)
+    # return a_cat_b[counts.gt(1)] # counts.gt returns a bool mask
+    # return a_cat_b[torch.where(counts.gt(1))] # where() returns the indexes of True elements
+    base_mask = torch.zeros_like(predicted).bool()
+    checked_tokens = []
+    for token in data:
+        if token in checked_tokens:
+            continue
+        checked_tokens.append(token)
+        base_mask += predicted.eq(token)
+    return predicted[base_mask]
+
+
+def differentiable_tensor_len(a: torch.Tensor) -> torch.Tensor:
+    """Calculates the length (hence first dimension) of the tensor in a differentiable way.
+    Only non-zero elements are considered, since 0 is the padding token id by default, otherwise all the input data
+    would have the same length.
+
+    Args:
+        a (torch.Tensor)
+
+    Returns:
+        torch.Tensor:
+    """
+    nonzero_a = a[a.nonzero()]
+    return torch.sum((nonzero_a)/(nonzero_a))
+
+
+def soft_argmax(a: torch.Tensor) -> torch.Tensor:
+    """A differentiable way of computing the argmax.
+    Assumes tensor of shape [batch_size, sequence_length, vocab_size]
+
+    https://stackoverflow.com/questions/46926809/getting-around-tf-argmax-which-is-not-differentiable
+    https://bouthilx.wordpress.com/2013/04/21/a-soft-argmax/
+
+    Args:
+        a (torch.Tensor): _description_
 
     Returns:
         torch.Tensor: _description_
     """
+    a_range = torch.arange(a.size(-1)).to(a.device)
+    return torch.sum(torch.softmax(a*1e10, -1)*a_range, -1)
+
+
+def semantic_fidelity_loss(source_data_ids, target_texts_ids, logits: List[str],
+                            missing_data_token_weight: Union[float, torch.Tensor] = 0.5,
+                            token_difference_weight: Union[float, torch.Tensor] = 0.5) -> torch.Tensor:
+    """A (theorically) differentiable implemenation of the semantic fidelity loss.
+    The loss is comprises two elements. For each triple (input data - input text - output text) we calculate
+        - the absolute difference between the number of data tokens and the number of data tokens
+            actually used in the predicted sentence (this is technically an approximation of said value,
+            given the implementation of tensor_intersection)
+        - the absolute difference between the input text and the output text lengths'
+    Both of those values are aggregated after a log is applied to them, to keep the same order of magnitude
+    as the cross entropy loss.
+
+    The final loss is calculated as a weighted average of said values.
+
+    Args:
+        source_data_ids ()
+        target_texts_ids ()
+        logits (): model outputs
+        missing_data_token_weight (Union[float, torch.Tensor], optional): Either a float or a single-item tensor. Defaults to 0.5.
+        token_difference_weight (Union[float, torch.Tensor], optional): Either a float or a single-item tensor. Defaults to 0.5.
+
+    Returns:
+        torch.Tensor: single-item tensor containing the calculated loss
+    """
     total_missing_data_tokens = []
     total_token_differences = []
-    stopwords = set(nltk.corpus.stopwords.words("english"))
-    for data, original, generated in zip(source_data, target_texts, model_outputs):
-        #* extract single data from complete data, except for yes/no values
-        data_tokens = list(re.findall(r"\[([\s\w\d]*)\]", data))
-        data_tokens = [token.strip() for token in data_tokens if not token.strip() in ["yes", "no"]]
-        #* check if data in target
-        missing_data_tokens = len(data_tokens)
-        for data_token in data_tokens:
-            if data_token in generated:
-                missing_data_tokens -= 1
-        missing_data_tokens = math.log(missing_data_tokens) if missing_data_tokens != 0 else 0
-        total_missing_data_tokens.append(missing_data_tokens)
+    predictions = soft_argmax(logits).floor() # cannot simply convert to int without losing grad
+    for data, target, predicted in zip(source_data_ids, target_texts_ids, predictions):
+        #* check how many data tokens are found in predicted
+        data = data.float()
+        source_intersect_predicted = tensor_intersection(data, predicted)
+        intersection_len = differentiable_tensor_len(source_intersect_predicted)
+        data_len = differentiable_tensor_len(data)
+        log_missing_data_tokens = torch.log((torch.abs(data_len - intersection_len))+0.1)
+        total_missing_data_tokens.append(log_missing_data_tokens)
         #* calculate token difference
-        tokenized_original = nltk.tokenize.word_tokenize(original)
-        tokenized_generated = nltk.tokenize.word_tokenize(generated)
-        #* retain only non-stopwords and non ,/. tokens
-        tokenized_original = [token for token in tokenized_original if token not in stopwords and token not in [",", "."]]
-        tokenized_original = [token for token in tokenized_generated if token not in stopwords and token not in [",", "."]]
-        token_difference = abs(len(tokenized_generated) - len(tokenized_original))
-        token_difference = math.log(token_difference) if token_difference != 0 else 0
-        total_token_differences.append(token_difference)
-    total_missing_data_tokens = torch.tensor(total_missing_data_tokens, dtype=torch.float64)
-    total_token_differences = torch.tensor(total_token_differences, dtype=torch.float64)
+        target_len = differentiable_tensor_len(target)
+        predicted_len = differentiable_tensor_len(predicted)
+        log_token_difference = torch.log(torch.abs(target_len - predicted_len)+0.1)
+        total_token_differences.append(log_token_difference)
+    total_missing_data_tokens = torch.stack(total_missing_data_tokens)
+    total_token_differences = torch.stack(total_token_differences)
     sf_loss = missing_data_token_weight * torch.mean(total_missing_data_tokens) + \
                 token_difference_weight * torch.mean(total_token_differences)
     return sf_loss
