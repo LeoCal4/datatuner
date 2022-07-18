Fixed softmax, SF, added PARENT,

src/datatuner/lm/custom/custom_loss.py
@@ -12,7 +12,6 @@ import torch.nn.functional as F
 from transformers.tokenization_utils import PreTrainedTokenizer
 
 
-
 logging.basicConfig(level=logging.INFO)
 log = logging.getLogger(__file__)
 
@@ -133,7 +132,7 @@ def semantic_fidelity_loss_with_confidences(source_data_ids: torch.Tensor, logit
         torch.Tensor
     """
     total_len_ratios = []
-    softmaxes = F.softmax(logits, dim=1)
+    softmaxes = F.softmax(logits, dim=-1)
     confidences, predictions = torch.max(softmaxes, -1)
     for data, predicted in zip(source_data_ids, predictions):
         source_intersect_predicted = tensor_intersection(data, predicted)
@@ -158,8 +157,9 @@ def word_based_semantic_fidelity_loss(
         torch.Tensor
     """
     total_len_ratios = []
-    softmaxes = F.softmax(logits, dim=1)
+    softmaxes = F.softmax(logits, dim=-1)
     _, predictions = torch.max(softmaxes, -1)
+    predictions = utils.crop_sentences_tensor_to_eos_token(predictions, tokenizer.eos_token_id)
     batch_sentences = tokenizer.batch_decode(predictions, skip_special_tokens=True)
     for data, target, predicted in zip(source_data_values, target_data, batch_sentences):
         #* get all the data values tokens, separating both different slots and slots where more values are separated by a comma
@@ -191,8 +191,9 @@ def word_based_semantic_fidelity_loss_with_confidences(
         torch.Tensor
     """
     total_len_ratios = []
-    softmaxes = F.softmax(logits, dim=1)
+    softmaxes = F.softmax(logits, dim=-1)
     confidences, predictions = torch.max(softmaxes, -1)
+    predictions = utils.crop_sentences_tensor_to_eos_token(predictions, tokenizer.eos_token_id)
     batch_sentences = tokenizer.batch_decode(predictions, skip_special_tokens=True)
     for data, target, predicted in zip(source_data_values, target_data, batch_sentences):
         #* get all the data values tokens, separating both different slots and slots where more values are separated by a comma
@@ -216,8 +217,9 @@ def word_based_semantic_fidelity_loss_with_confidences(
 
 
 def calculate_dcs(logits: torch.Tensor, original_data: List[str], tokenizer: PreTrainedTokenizer, dataset_name: str) -> torch.Tensor:
-    softmaxes = F.softmax(logits, dim=1)
+    softmaxes = F.softmax(logits, dim=-1)
     confidences, predictions = torch.max(softmaxes, -1)
+    predictions = utils.crop_sentences_tensor_to_eos_token(predictions, tokenizer.eos_token_id)
     batch_sentences = tokenizer.batch_decode(predictions, skip_special_tokens=True)
     ser_value, _ = ser_calculator.calculate_ser(original_data, batch_sentences, dataset_name)
     mean_conf = torch.mean(confidences)

src/datatuner/lm/custom/train.py
@@ -188,23 +188,23 @@ def main():
                 progress_bar.update(batch_size)
                 loss_val = loss.item() # get the item since loss is a tensor
                 progress_bar.set_postfix(epoch=epoch, loss=loss_val)
-                if batch_num == 0 or batch_num % (batch_size*100) == 0:
-                    softmaxes = F.softmax(logits, dim=-1)
-                    predictions = torch.argmax(softmaxes, -1)
-                    new_pred = []
-                    for pred in predictions.tolist():
-                        if not pred:
-                            continue 
-                        try:
-                            new_pred.append(pred[:pred.index(tokenizer.eos_token_id)])
-                        except ValueError:
-                            new_pred.append(pred)
-                    sentences = tokenizer.batch_decode(new_pred, skip_special_tokens=False)
-                    del predictions
-                    source_data = '\n'.join(batch['source_data_values'][0:5])
-                    log.info(f"\nSource:\n{source_data}")
-                    generated = '\n'.join(sentences[0:5])
-                    log.info(f"\nGenerated:\n{generated}")
+                # if batch_num == 0 or batch_num % (batch_size*100) == 0:
+                #     softmaxes = F.softmax(logits, dim=-1)
+                #     predictions = torch.argmax(softmaxes, -1)
+                #     new_pred = []
+                #     for pred in predictions.tolist():
+                #         if not pred:
+                #             continue 
+                #         try:
+                #             new_pred.append(pred[:pred.index(tokenizer.eos_token_id)])
+                #         except ValueError:
+                #             new_pred.append(pred)
+                #     sentences = tokenizer.batch_decode(new_pred, skip_special_tokens=False)
+                #     del predictions
+                #     source_data = '\n'.join(batch['source_data_values'][0:5])
+                #     log.info(f"\nSource:\n{source_data}")
+                #     generated = '\n'.join(sentences[0:5])
+                #     log.info(f"\nGenerated:\n{generated}")
                 del logits
 
         #* evaluate
@@ -282,7 +282,7 @@ def main():
         json.dump(predictions, f, sort_keys=False, indent=4, ensure_ascii=False)
     #* calculate metrics
     metrics_compendium = metrics.create_metrics_compendium(
-        predictions,
+        best_predictions,
         precomputed_ser=last_ser_values,
         precomputed_bleu=last_avg_bleu
     )